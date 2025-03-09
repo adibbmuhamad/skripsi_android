d8215b943203e26a5808be3a19fef7197d8a19f2
Muhamad Adib Minanurohman <inanadib085@gmail.com>
2025-03-10 04:02:26 +0700

    Refactor MainActivity and add dependencies
    Removes the `Greeting` composable and its preview from `MainActivity.kt`.
    Adds several dependencies to `build.gradle.kts`, including:
    - Navigation Compose
    - Compose animation and Material icons
    - Retrofit and related libraries
    - Coil Compose
    - Lifecycle dependencies
    - Accompanist System UI Controller

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,37 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation("androidx.navigation:navigation-compose:2.8.4")
+    implementation("androidx.compose.animation:animation:1.7.5")
+    implementation("androidx.compose.material:material-icons-extended:1.7.6")
+
+    //Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.0-alpha05")
+
+    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.compose.ui:ui:1.5.1")
+    implementation("androidx.compose.material:material:1.5.1")
+    implementation("io.coil-kt:coil-compose:2.4.0")
+
+    // Untuk penggunaan LiveData dengan Kotlin Coroutines (disarankan)
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.3")
+
+    // Atau, jika tidak menggunakan Kotlin Coroutines
+    // implementation("androidx.lifecycle:lifecycle-livedata:2.8.3")
+
+    // Jika kamu menggunakan ViewModel, tambahkan juga ini
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.3")
+
+    // Jika kamu menggunakan lifecycle-runtime-compose
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.3")
+
+    //SystemUiController
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.24.13-rc")
+
+    implementation("androidx.navigation:navigation-compose:2.8.4")
+
 }
