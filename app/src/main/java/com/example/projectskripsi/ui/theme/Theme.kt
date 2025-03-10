17c5bf8f6be7664101b414463a712f4e78ea474c
Muhamad Adib Minanurohman <inanadib085@gmail.com>
2025-03-11 01:37:10 +0700

    Set status bar color and light status bar icons
    -   Sets the status bar color to the primary color of the color scheme.
    -   Configures light status bar icons for devices running Android M (API level 23) and above.
    -   Uses `SideEffect` to update the status bar color and icons.

--- app/src/main/java/com/example/projectskripsi/ui/theme/Theme.kt
+++ app/src/main/java/com/example/projectskripsi/ui/theme/Theme.kt
@@ -2,14 +2,16 @@ package com.example.projectskripsi.ui.theme
 
 import android.app.Activity
 import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.ViewCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -45,6 +47,16 @@ fun ProjectSkripsiTheme(
             dynamicLightColorScheme(context) // Always use light dynamic color scheme
         }
         else -> LightColorScheme // Always use light color scheme
+    }
+
+    val view = LocalView.current
+    SideEffect {
+        val window = (view.context as? Activity)?.window
+        window?.statusBarColor = colorScheme.primary.toArgb() // Set status bar color
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val insetsController = ViewCompat.getWindowInsetsController(view)
+            insetsController?.isAppearanceLightStatusBars = true // Set light status bar icons
+        }
     }
 
     MaterialTheme(
