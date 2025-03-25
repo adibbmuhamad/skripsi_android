079ad779371ac55e46f28d85e6948203eae709bf
Muhamad Adib Minanurohman <inanadib085@gmail.com>
2025-03-26 01:15:00 +0700

    Update Retrofit base URL and add fields to HealthReport model
    -   Updates the base URL in `RetrofitClient.kt` from `http://192.168.30.69:8000/` to `http://192.168.21.189:8000/`.
    -   Adds `report_date`, `health_status`, `symptoms`, `doctors_notes`, and `attachments` fields to the `HealthReport` data class in `StudentDetailResponse.kt`.

--- app/src/main/java/com/example/projectskripsi/data/network/RetrofitClient.kt
+++ app/src/main/java/com/example/projectskripsi/data/network/RetrofitClient.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
 
-    private const val BASE_URL = "http://192.168.30.69:8000/" // Gantilah dengan URL API yang benar
+    private const val BASE_URL = "http://192.168.21.189:8000/" // Gantilah dengan URL API yang benar
 
     // Membuat instance HttpLoggingInterceptor
     private val loggingInterceptor = HttpLoggingInterceptor().apply {
